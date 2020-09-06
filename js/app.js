748bd7292a7b49e55c7a03b89b9da1cce85f463f
mravinshu <55399637+mravinshu@users.noreply.github.com>
2020-09-06 18:36:23 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,14 @@
+const navSlide = () => {
+    const menu = document.querrySelector('.menu');
+    const nav = document.querySelector('nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    menu.addEventListner('click', () => {
+        nav.classList.toggle('nav-active');
+    });
+
+    navLinks.forEach(link, index)=> ({
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7}s`
+    });
+}
+
+navSlide();
